81c4664fcbdb2c0cc73b2ab7165afcec810d7a4d
masarabbit <masahitoart@gmail.com>
2022-05-19 00:49:56 +0100

    add motion, hunt and flee

--- time/app.js
+++ time/app.js
@@ -1,24 +1,27 @@
 function init() {  
 
-  // TODO add move about
-  // TODO add number display (do with before?)
+  // TODO add way to absorb other bots when overlapped and time is higher.
+  // TODO when certain distance away from other all other bots, maybe stop motion
 
   const body = document.querySelector('.wrapper')
+  const n = 16
+  const defaultTime = 99
   const bots = []
   const botData = {
     interval: null,
-    x: 0,
-    y: 0,
     stop: true,
     frameSpeed: 100,
     animation: 'walk',
     frame: 0,
     frameTimer: null,
+    mode: 'hunter',
+    time: defaultTime,
   }
   const cellD = 32
 
   const animationFrames = {
     walk: ['n-1', 'n-2', 'n-1', 'r-1', 'r-2', 'r-1'],
+    stop: ['n-0']
     // stop: [0],
     // celebrate: [5, 6, 7, 6, 3, 4],
     // turnFromup: [0, 1, 2, 3, 4],
@@ -29,7 +32,7 @@ function init() {
   }
 
   const setMargin = (target, x, y) => target.style.transform = `translate(${x}px, ${y}px)`
-  const randomP = max => Math.ceil(Math.random() * max)
+  const randomN = max => Math.ceil(Math.random() * max)
 
   const startBot = (bot, data) =>{
     animate(bot, data)
@@ -37,11 +40,12 @@ function init() {
   }
   
   const animate = (bot, data) =>{
-    const { frame:i, animation, frameSpeed } = data
+    const { frame:i, animation, frameSpeed, mode } = data
     const sprite = bot.childNodes[0].childNodes[0]
     const frame = animationFrames[animation][i].split('-')
     setMargin(sprite, `-${frame[1] * cellD}`, 0)
     bot.childNodes[0].classList[frame[0] === 'n' ? 'remove' : 'add']('flip')
+    bot.className = `bot_wrapper ${mode}`
     data.frame = i === animationFrames[animation].length - 1 ? 0 : i + 1
     data.frameTimer = setTimeout(()=> animate(bot, data), frameSpeed)
   }
@@ -55,20 +59,36 @@ function init() {
     const bot = document.createElement('div')
     bot.classList.add('bot_wrapper')
     bot.innerHTML = '<div><div class="bot"></div></div>'
-    bot.setAttribute('time', 99)
     body.appendChild(bot)
     bots.push({...botData})
     const data = bots[bots.length - 1]
+    data.time = randomN(defaultTime)
     data.bot = bot
-    data.x = x
-    data.y = y
+    data.xy = { x, y }
+    data.pos = {
+        x: x + n,
+        y: y + n
+      }
+    bot.setAttribute('index', bots.length -1)
+    bot.setAttribute('time', data.time)
     setMargin(bot, x, y)
     startBot(bot, data)
   }
+
+  const testPos = [
+    [100, 100],
+    [250, 250],
+    [300, 100],
+    [400, 100],
+  ]
   
-  new Array(5).fill('').map(()=>{
-    return [randomP(body.clientWidth - 100), randomP(body.clientHeight - 100)]
-  }).forEach( pos => {
+  // new Array(5).fill('').map(()=>{
+  //   return [randomN(body.clientWidth - 100), randomN(body.clientHeight - 100)]
+  // }).forEach( pos => {
+  //   createBot(pos[0], pos[1])
+  // })
+
+  testPos.forEach( pos => {
     createBot(pos[0], pos[1])
   })
 
@@ -81,17 +101,74 @@ function init() {
   // console.log(bots)
 
   const stopBot = (animation, data) =>{
-    changeAnimation(animation)
+    changeAnimation(animation, data)
     data.stop = true
+    clearTimeout(data.frameTimer)
   }
+  
+  const checkBoundaryAndUpdatePos = (x, y, data) =>{
+    // TODO not working for some reason
+    const lowerLimit = -100 // buffer from window edge
+    const upperLimit = 100
+    
+    if (x > lowerLimit && x < (body.clientWidth - upperLimit)){
+      data.xy.x = x
+    } 
+    if (y > lowerLimit && y < (body.clientHeight - upperLimit)){
+      data.xy.y = y
+    }
+    setMargin(data.bot, data.xy.x, data.xy.y)
+  }
+
+
+  const distanceBetween = (a, b) =>{
+    return Math.sqrt(Math.pow((a.x - b.x), 2) + Math.pow((a.y - b.y), 2))
+  }
+
+  const moveBots = () =>{
+    bots.forEach((b, i) =>{
+      if (!b.stop){
+        const distances = bots.map((bot, index) =>{
+          return {
+            index,
+            time: bot.time,
+            distance: distanceBetween(b.pos, bot.pos)
+          }
+        })
+        const closestBot = bots[[...distances.filter(d => d.index !== i)].sort((a, b) => a.distance - b.distance )[0].index]
+        b.mode = b.time >= closestBot.time ? 'hunter' : 'flee'
+        const seedDistance = randomN(20) + 10
+        const distance = b.mode === 'hunter' ? seedDistance : -seedDistance
+        const xy = {
+          x: b.xy.x > closestBot.xy.x ? b.xy.x - distance : b.xy.x + distance,
+          y: b.xy.y > closestBot.xy.y ? b.xy.y - distance : b.xy.y + distance
+        }
+  
+        checkBoundaryAndUpdatePos(xy.x, xy.y, b)
+        // setMargin(b.bot, b.xy.x, b.xy.y)
+        // const { xy: { x, y } } = b
+        b.pos = {
+          x: b.xy.x + n,
+          y: b.xy.y + n,
+        }
+      }
+    })
+  } 
 
   setInterval(()=>{
     bots.forEach(bot =>{
-      bot.bot.setAttribute('time', +bot.bot.getAttribute('time') - 1)
+      if (!bot.stop) {
+        bot.time--
+        bot.bot.setAttribute('time', bot.time)
+        if (bot.time <= 0) {
+          stopBot('stop', bot)
+          console.log(bot.bot)
+          bot.bot.className = 'bot_wrapper'
+        }
+      }
     })
+    moveBots()
   }, 1000)
-
-
 }
 
 window.addEventListener('DOMContentLoaded', init)
