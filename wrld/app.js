f8245608265ef809ae81edcf84fb650934c870ff
masarabbit <masahitoart@gmail.com>
2022-07-27 00:26:33 +0100

    add logic for multiple circle

--- wrld/app.js
+++ wrld/app.js
@@ -11,31 +11,49 @@ function init() {
     'u': 0,
     'd': 0,
   }
+  const indexConfig = {
+    'l': -1,
+    'r': 1
+  }
+
+  const elements = {
+    elements: null
+  }
+
+  const indexs = {}
+  
 
   const bearData = {
     animationTimer: ['',''],
     sprite: null,
     frameOffset: 0
   }
-
+  
+  const indicator = document.querySelector('.indicator')
   const cellD = 32
   const circle = document.querySelector('.circle')
   const circleWrapper = document.querySelector('.circle_wrapper')
-  const elements = ['navy', 'black', 'white','navy', 'black', 'white','navy', 'black', 'white']
-  const placeElements = elements =>{
-    elements.forEach((ele, i)=>{
+  const mapItems = ['navy', 'black', 'white','navy', 'black', 'white','navy', 'black', 'white','navy', 'black', 'white']
+  const placeElements = mapItems =>{
+    mapItems.forEach((ele, i)=>{
       const element = document.createElement('div')
       element.style.backgroundColor = ele
       element.classList.add('element')
       circle.append(element)
+
+      // element.style.transform = `translate(${200 - (50 / 2)}px, -${55}px) rotate(${i * 30}deg)`
       setTimeout(()=>{
         element.style.transform = `translate(${200 - (50 / 2)}px, -${55}px) rotate(${i * 30}deg)`
       }, 1000)
       element.innerHTML = i
       element.style.transformOrigin = `center ${200 + 55}px`
     })
+    elements.elements = document.querySelectorAll('.element')
+    updateElements()
   }
 
+  // mapItems.forEach((_,i) => indexs[i] = i < 6 ? 0 : 2)
+  mapItems.forEach((_,i) => indexs[i] = 0)
 
 
   const setSpritePos = (num, actor, sprite) =>{
@@ -60,6 +78,40 @@ function init() {
     actor.animationTimer.forEach(timer=> clearTimeout(timer))
     setSpritePos(m, actor, sprite)
   }
+  
+
+  const updateElements = () =>{
+    elements.elements.forEach((element, i)=> {
+      const angle = i * 30 + circleData.angle
+      const newAngle = angle >= 360 
+        ? angle - (Math.floor(angle / 360) * 360)
+        : angle < 0
+          ? angle - (Math.floor(angle / 360) * 360)
+          : angle
+
+      if ((newAngle === 180) && ['l', 'r'].includes(circleData.key)) {
+        indexs[i] += indexConfig[circleData.key]
+      }
+      // indexs[i] + Math.floor(circleData.angle / -360)
+      indexs[i] = indexs[i] === -1 
+        ? 2
+        : indexs[i] === 3
+          ? 0
+          : indexs[i]
+
+      // TODO adjust index based on angle    
+
+      element.innerHTML = `<p>[${i}]</p><p>${newAngle}</p><p><${indexs[i]}></p>`
+      element.className = `element color-${indexs[i]}`
+    })
+    indicator.innerHTML = `${circleData.angle} ${Math.floor(circleData.angle / -360)}`
+  }
+
+  const rotateCircle = key =>{
+    circleData.angle += config[key]
+    circle.style.transform = `rotate(${circleData.angle}deg)`
+    updateElements()
+  }
 
   const handleKey = e =>{
     const key = e.key.replace('Arrow','').toLowerCase()[0]
@@ -70,8 +122,7 @@ function init() {
         if (!circleData.key) {
           clearInterval(circleData.interval)
         } else {
-          circleData.angle += config[key]
-          circle.style.transform = `rotate(${circleData.angle}deg)`
+          rotateCircle(key)
           turnSprite({ e:circleData.key, actor: bearData, animate: true})
         }
       }, 100)
@@ -89,7 +140,7 @@ function init() {
 
 
   
-  placeElements(elements)
+  placeElements(mapItems)
   placeBear()
 
   window.addEventListener('keydown', e => handleKey(e))
@@ -97,6 +148,15 @@ function init() {
     turnSprite({ e:circleData.key, actor: bearData })
     circleData.key = null
   })
+  
+  // setInterval(()=>{
+  //   rotateCircle('l')
+  //   // rotateCircle('l')
+  //   // rotateCircle('l')
+  //   // rotateCircle('l')
+  //   // rotateCircle('l')
+  // }, 1000)
+
 
 }
 
