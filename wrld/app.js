1d206538fd94ad6f58f07a97172a3ced4a378984
masarabbit <masahitoart@gmail.com>
2022-07-29 21:08:13 +0100

    use different touch control

--- wrld/app.js
+++ wrld/app.js
@@ -17,17 +17,25 @@ function init() {
     'd': 0,
   }
 
+  const touchControl = {
+    active: false,
+    timer: null,
+    direction: null,
+  }
+  
+
+  const setTargetPos = (target, x, y) => Object.assign(target.style, { left: `${x}px`, top: `${y}px` })
   
 
   const addEvents = (target, event, action, array) =>{
     array.forEach(a => event === 'remove' ? target.removeEventListener(a, action) : target.addEventListener(a, action))
   }
   const mouse = {
-    up: (t, e, a) => addEvents(t, e, a, ['mouseup', 'touchend', 'touchcancel']),
-    // move: (t, e, a) => addEvents(t, e, a, ['mousemove', 'touchmove']),
+    up: (t, e, a) => addEvents(t, e, a, ['mouseup', 'touchend']),
+    move: (t, e, a) => addEvents(t, e, a, ['mousemove', 'touchmove']),
     down: (t, e, a) => addEvents(t, e, a, ['mousedown', 'touchstart']),
-    // enter: (t, e, a) => addEvents(t, e, a, ['mouseenter', 'touchstart']),
-    // leave: (t, e, a) => addEvents(t, e, a, ['mouseleave', 'touchmove'])
+    enter: (t, e, a) => addEvents(t, e, a, ['mouseenter', 'touchstart']),
+    leave: (t, e, a) => addEvents(t, e, a, ['mouseleave', 'touchmove'])
   }
   
 
@@ -261,8 +269,8 @@ function init() {
     updateElements()
   }
 
-  const handleKey =({ e, letter })=>{
-    const key = e && e.key.replace('Arrow','').toLowerCase()[0] || letter
+  const handleKey = ({ e, letter }) =>{
+    const key = e?.key.replace('Arrow','').toLowerCase()[0] || letter
     if (!['l','r','u','d'].includes(key)) return
     if (circleData.key !== key){
       clearInterval(circleData.interval)
@@ -306,17 +314,74 @@ function init() {
     circleData.key = null
   })
 
-  arrows.forEach(arrow =>{
-    mouse.down(arrow, 'add', e =>{
-      handleKey({ letter: e.target.dataset.d })
-    })
-    mouse.up(arrow, 'add', () =>{
-      turnSprite({ e:circleData.key, actor: bearData })
-      circleData.key = null
-    })
-  })
-  
+  const distanceBetween = (a, b) => Math.round(Math.sqrt(Math.pow((a.x - b.x), 2) + Math.pow((a.y - b.y), 2)))
 
+const drag = (target, pos, x, y) =>{
+  pos.a = pos.c - x
+  pos.b = pos.d - y
+  const newX = target.offsetLeft - pos.a
+  const newY = target.offsetTop - pos.b
+  if (distanceBetween({x: 0,y: 0}, {x: newX, y: newY}) < 35) {
+    setTargetPos(target, newX, newY)
+    touchControl.direction = Math.abs(newX) < Math.abs(newY)
+      ? newY < 0 ? 'u' : 'd'
+      : newX < 0 ? 'l' : 'r'
+  }  
+}
+
+const client = (e, type) => e.type[0] === 'm' ? e[`client${type}`] : e.touches[0][`client${type}`]
+const roundedClient = (e, type) => Math.round(client(e, type))
+
+const addTouchAction = (target, handleKeyAction) =>{
+  const pos = { a: 0, b: 0, c: 0, d: 0 }
+  
+  const onGrab = e =>{
+    pos.c = roundedClient(e, 'X')
+    pos.d = roundedClient(e, 'Y')  
+    mouse.up(document, 'add', onLetGo)
+    mouse.move(document, 'add', onDrag)
+    touchControl.active = true
+    touchControl.timer = setInterval(()=> {
+      if (touchControl.active) handleKeyAction({ letter:touchControl.direction })
+    }, 200)
+  }
+  const onDrag = e =>{
+    const x = roundedClient(e, 'X')
+    const y = roundedClient(e, 'Y')
+    drag(target, pos, x, y)
+    pos.c = x
+    pos.d = y
+  }
+  const onLetGo = () => {
+    mouse.up(document, 'remove', onLetGo)
+    mouse.move(document,'remove', onDrag)
+    target.style.transition = '0.2s'
+    setTargetPos(target, 0 ,0)
+    setTimeout(()=>{
+      target.style.transition = '0s'
+    }, 200)
+    clearInterval(touchControl.timer)
+    touchControl.active = false
+    
+    turnSprite({ e:circleData.key, actor: bearData })
+    circleData.key = null
+  }
+  mouse.down(target,'add', onGrab)
+}
+
+const control = document.querySelector('.touch_circle')
+addTouchAction(control, handleKey)
+  // arrows.forEach(arrow =>{
+  //   mouse.down(arrow, 'add', e =>{
+  //     handleKey({ letter: e.target.dataset.d })
+  //   })
+  //   mouse.up(arrow, 'add', () =>{
+  //     turnSprite({ e:circleData.key, actor: bearData })
+  //     circleData.key = null
+  //   })
+  // })
+  
+  
 
 }
 
