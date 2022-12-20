37cd3684ad06926f4f21ac960400ce77c9490976
masarabbit <masahitoart@gmail.com>
2022-12-20 23:51:04 +0000

    update missile logic

--- catzilla/app.js
+++ catzilla/app.js
@@ -3,7 +3,8 @@
 
     const elements = {
       indicator: document.querySelector('.indicator'),
-      wrapper: document.querySelector('.wrapper')
+      wrapper: document.querySelector('.wrapper'),
+      target: document.querySelector('.target')
     }
 
     const px = num => `${num}px`
@@ -16,22 +17,39 @@
       if (isNum(deg)) target.style.transform = `rotate(${deg}deg)`
     }
 
-
+    const radToDeg = rad => Math.round(rad * (180 / Math.PI))
     const isNum = x => typeof x === 'number'
+
+    const control = {
+      x: null,
+      y: null,
+      angle: 360,
+    }
+
+    const nearestN = (n, denom) =>{
+      return n === 0 ? 0 : (n - 1) + Math.abs(((n - 1) % denom) - denom)
+    }
+    
+    // const clickedAngle = () =>{
+    //   const angle = radToDeg(Math.atan2(penguinData.pos.y - control.y, penguinData.pos.x - control.x)) - 90
+    //   const adjustedAngle = angle < 0 ? angle + 360 : angle
+    //   return nearestN(adjustedAngle, 45)
+    // }
+
+
 
     elements.indicator.innerHTML = 'test'
     console.log('test')
     
     window.addEventListener('keydown', e => {
-      console.log(e.key)
+      // console.log(e.key)
       if (e.key === 'Enter') {
         const missile = document.createElement('div')
         missile.classList.add('missile')
         missile.style.transition = '1s'
         const { height, width } = elements.wrapper.getBoundingClientRect()
-        console.log(height, width)
         const missileData = {
-          deg: -45,
+          deg: 0,
           x: 0,
           y: height - 30,
         }
@@ -41,9 +59,15 @@
           x, y, deg
         })
         setInterval(()=>{
-          missileData.x = missileData.x + 20
-          missileData.y = missileData.y - 10
-          missileData.deg = missileData.deg - 5
+          // missileData.x = missileData.x + 20
+          // missileData.y = missileData.y - 10
+          missileData.x = missileData.x + (control.x > missileData.x ? 20 : -20)
+          missileData.y = missileData.y + (control.y > missileData.y ? 20 : -20)
+          const angle = radToDeg(Math.atan2(missileData.y - control.y, missileData.x - control.x))
+          const adjustedAngle = angle < 0 ? angle + 360 : angle
+          missileData.deg = adjustedAngle
+
+          // adjust based on target?
           const { x, y, deg } = missileData 
           elements.indicator.innerHTML = `x: ${x} y: ${y} deg: ${deg}`
           setStyles({
@@ -53,6 +77,19 @@
         }, 100)
         elements.wrapper.append(missile)
       }
+    })
+
+    elements.wrapper.addEventListener('click', e =>{
+      // const target = document.createElement('div')
+      control.x = e.clientX - 20
+      control.y = e.clientY - 20
+      setStyles({
+        target: elements.target,
+        x: control.x,
+        y: control.y
+      })
+      console.log(e)
+  
     })
   }
   
