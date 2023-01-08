1c2febdaccc2a52475c544cf1054507d067a9b7a
masarabbit <masahitoart@gmail.com>
2023-01-08 17:35:42 +0000

    slightly improve missile

--- catzilla/app.js
+++ catzilla/app.js
@@ -7,6 +7,7 @@ function init() {
     target: document.querySelector('.target')
   }
 
+
   const px = num => `${num}px`
     
   const setStyles = ({ target, h, w, x, y, deg }) =>{
@@ -17,6 +18,7 @@ function init() {
     if (isNum(deg)) target.style.transform = `rotate(${deg}deg)`
   }
 
+  const overBuffer = ({ a, b, buffer }) => Math.abs(a - b) > buffer
   const radToDeg = rad => Math.round(rad * (180 / Math.PI))
   const isNum = x => typeof x === 'number'
 
@@ -30,16 +32,41 @@ function init() {
     return n === 0 ? 0 : (n - 1) + Math.abs(((n - 1) % denom) - denom)
   }
     
+  const adjustAngle = angle => {
+    const adjustedAngle = angle % 360
+    return adjustedAngle < 0 
+      ? adjustedAngle + 360 
+      : adjustedAngle
+  }
+
   // const clickedAngle = () =>{
   //   const angle = radToDeg(Math.atan2(penguinData.pos.y - control.y, penguinData.pos.x - control.x)) - 90
   //   const adjustedAngle = angle < 0 ? angle + 360 : angle
   //   return nearestN(adjustedAngle, 45)
   // }
 
+const getDirection = ({x, y, xSpeed, ySpeed, targetX, targetY}) => {
+	const cross = xSpeed * (targetY - y) - ySpeed * (targetX - x)
+	return cross > 0 ? 'left' : 'right'
+}
 
+// const rotateVector = ({ dx, dy, rotate }) =>
+// {
+// 	const rot = rotate * ( Math.PI() / 180 )
+
+// 	const rx = Math.cos(rot) * dx - Math.sin(rot) * dy
+// 	const ry = Math.sin(rot) * dx + Math.cos(rot) * dy
+
+// 	dx = rx
+// 	dy = ry
+// }
+
+const config = {
+  left: -20,
+  right: 20
+}
 
   elements.indicator.innerHTML = 'test'
-  console.log('test')
     
   window.addEventListener('keydown', e => {
     // console.log(e.key)
@@ -61,23 +88,30 @@ function init() {
         x, y, deg
       })
       setInterval(()=>{
-        // missileData.x = missileData.x + 20
-        // missileData.y = missileData.y - 10
-        missileData.x = missileData.x + (control.x > missileData.x ? 20 : -20)
-        missileData.y = missileData.y + (control.y > missileData.y ? 20 : -20)
-        const angle = radToDeg(Math.atan2(missileData.y - control.y, missileData.x - control.x)) - 90
-        const adjustedAngle = angle < 0 ? angle + 360 : angle
-        console.log('test', Math.abs(adjustedAngle - missileData.deg))
-        // if ( )
-        // TODO need some way to rotate the correct way when turning between number close to 0 and number close to 360
-        // missileData.deg = Math.abs(adjustedAngle - missileData.deg) > 270 
-        //   ? 360 - adjustedAngle
-        //   : adjustedAngle
-        missileData.deg = adjustedAngle
+        if (overBuffer({ a: missileData.x, b:control.x, buffer: 10})) missileData.x = missileData.x + (control.x > missileData.x ? 20 : -20)
+        if (overBuffer({ a: missileData.y, b:control.y, buffer: 10})) missileData.y = missileData.y + (control.y > missileData.y ? 20 : -20)
 
-        // adjust based on target?
+
+        const direction = getDirection({
+          targetX: control.x,
+          targetY: control.y,
+          xSpeed: 5,
+          ySpeed: 5,
+          x: missileData.x,
+          y: missileData.y
+        })
+
+        const angle = radToDeg(Math.atan2(missileData.y - control.y, missileData.x - control.x)) - 90
+        const adjustedAngle = adjustAngle(angle)
+
+        const diff = Math.abs((adjustedAngle) - adjustAngle(missileData.deg))
+
+        if (diff >= 20) missileData.deg = adjustAngle(missileData.deg + config[direction])
+
+        // target
+        elements.indicator.innerHTML = `targetAngle: ${adjustedAngle} | missileAngle: ${missileData.deg} |adjustedMissileAngle: ${adjustAngle(missileData.deg)} | diff: ${diff} | config: ${config[direction]}`
+
         const { x, y, deg } = missileData 
-        elements.indicator.innerHTML = `x: ${x} y: ${y} deg: ${deg}`
         setStyles({
           target: missile,
           x, y, deg
@@ -96,7 +130,7 @@ function init() {
       x: control.x,
       y: control.y
     })
-    console.log(e)
+    // console.log(e)
   
   })
 }
