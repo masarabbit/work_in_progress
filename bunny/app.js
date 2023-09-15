6de58adc68a82d3e2e5eeb56c55aff1823b78392
masarabbit <masahitoart@gmail.com>
2023-09-16 00:22:55 +0200

    update bunny

--- bunny/app.js
+++ bunny/app.js
@@ -5,6 +5,7 @@ function init() {
     neck: document.querySelector('.neck-base'),
     shoulders: document.querySelectorAll('.shoulder'),
     elbows: document.querySelectorAll('.elbow'),
+    waist: document.querySelector('.belly-joint'),
   }
 
   const poses = {
@@ -15,22 +16,108 @@ function init() {
       },
       {
         el: parts.shoulders[0],
+        deg: 65
+      },
+      {
+        el: parts.elbows[0],
+        deg: 40
+      },
+      {
+        el: parts.shoulders[1],
+        deg: -65
+      },
+      {
+        el: parts.elbows[1],
+        deg: -40
+      },
+    ],
+    leftArmUp: [
+      {
+        el: parts.neck,
+        deg: 10
+      },
+      {
+        el: parts.shoulders[0],
+        deg: 100
+      },
+      {
+        el: parts.elbows[0],
         deg: 45
       },
       {
         el: parts.shoulders[1],
-        deg: -45
-      },
-      {
-        el: parts.elbows[0],
-        deg: 20
+        deg: -50
       },
       {
         el: parts.elbows[1],
-        deg: -20
+        deg: 65
       },
-    ]
+      {
+        el: parts.waist,
+        deg: 5
+      },
+    ],
+    rightArmUp: [
+      {
+        el: parts.neck,
+        deg: -10
+      },
+      {
+        el: parts.shoulders[0],
+        deg: 50
+      },
+      {
+        el: parts.elbows[0],
+        deg: -65
+      },
+      {
+        el: parts.shoulders[1],
+        deg: -100
+      },
+      {
+        el: parts.elbows[1],
+        deg: -45
+      },
+      {
+        el: parts.waist,
+        deg: -5
+      },
+    ],
+    bothArmsUp: [
+      {
+        el: parts.shoulders[0],
+        deg: 120
+      },
+      {
+        el: parts.elbows[0],
+        deg: 45
+      },
+      {
+        el: parts.shoulders[1],
+        deg: -120
+      },
+      {
+        el: parts.elbows[1],
+        deg: -45
+      },
+    ],
   }
+
+  poses.neutral = Object.keys(parts).map(part => {
+    return parts[part].length
+      ? Array.from(parts[part]).map(p => {
+        return { el:p, deg: 0 }
+      })
+      : { el: parts[part], deg: 0 }
+  }).flat(1)
+  
+
+  document.querySelector('.buttons').innerHTML =  Object.keys(poses).map(pose => `<button>${pose}</button>`).join('')
+  
+  document.querySelectorAll('button').forEach(b => b.addEventListener('click', ()=> {
+    pose('neutral')
+    pose(b.innerHTML)
+  }))
 
 
   const setStyles = ({ el, x, y, deg }) =>{
@@ -44,7 +131,7 @@ function init() {
     })
   }
   
-  pose('shrug')
+
 }
   
 window.addEventListener('DOMContentLoaded', init)
