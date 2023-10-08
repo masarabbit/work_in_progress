c4e5b50110f53fda0d94e85fbe72ee49e1c32760
masarabbit <masahitoart@gmail.com>
2023-10-09 00:27:53 +0200

    update isoDraw - add window scroll offsets, erase and layer toggles)

--- isoDraw/scripts/data.js
+++ isoDraw/scripts/data.js
@@ -1,8 +1,39 @@
+const elements = {
+  canvas: {
+    el: document.querySelector('canvas'),
+    ctx: function() {
+      return this.el.getContext('2d')
+    }
+  },
+  line: document.querySelector('.line'),
+  cube: document.querySelector('.cube'),
+  stair: document.querySelector('.stair'),
+  blank: document.querySelector('.blank'),
+  btns: document.querySelectorAll('.btn'),
+  indicator: document.querySelector('.indicator'),
+  cellData: document.querySelectorAll('.cells'),
+  stamp: document.querySelector('.stamp'),
+  palette: document.querySelector('.palette'),
+  max: document.querySelector('.max'),
+  layers: document.querySelector('.layers'),
+}
+
 const tiles = {
   stair: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAlCAYAAAAqXEs9AAAAAXNSR0IArs4c6QAAAUtJREFUWEfF11ESgyAMBNBy/0PjoIYRSsgmG9t+dUbEZwKrlk/Or97TFHY6doITUuvlKaVPF543euIAmavCwLygLUSBua6BDo5AxIde42q7sQhDkGlNWdcYCNpgCtJLcy1yCpQCyQClQlhQlZ5boSbb2Tve07bSNNaFrONaDkUC8wRNJf5KXm9FtPFIYA4gDfZSK5e7bwmS55JVmWgrdw/iECgKQQLTBWIhSBxAoCwIDZIJrLXk3Qz3DeYvai9kHr8KTLVlzzdAK1eiFVzl0hb0j8CEQGxrPBV0gX4RmKkgNh7a+SkgFvJcEhQoC0KD3gzMlpbwGyO6qJldKfGd+k4daKX6DU7BGIj1VUnBgDdM9VvN+oiDYK0ySBqXx8NLQ1ugvqHan91DdgdCIFbLtBtYVkyrkAcSBS0rNoMiEBY0wKSdDCQLZG0o9/EDEKlBLgROJT4AAAAASUVORK5CYII=',
   cube: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAlCAYAAAAqXEs9AAAAAXNSR0IArs4c6QAAAR1JREFUWEft2MESwiAMBFD5/4/GoRUmUmCTbFoP4rWSvi6RMU2vmE/+lElsObbAAcn59KTUyrnrehd+QfpUGJgVtIREwLQgE4SBIRAF8cBmoFCIBdaDboVoYBX0KGQFK6BczxH2UGPXl+PiANVCv4KJc+sE9SftU7AKkfcfbln/RXYrZj3TP3jbslki0TBUD4Lqk6FCKEHtejXIC9NCZH3Xzx7dCF2fJWpOCDWnF0InhGCop25LSBYu6bDnF71lG2Tphb1lKK2d0F8ndEzDciRGaYyuMz0kJ92wqcMDGo3cYXOZBbSa/cMmVw1I8xICgdpAgnpsBdJA2l8QYwNPm38EskC8oGliEuSBsKALTCDKNW0rXDbIvbCrFPaO8Q2E4usT3uthmwAAAABJRU5ErkJggg==',
 }
 
+const settings = {
+  column: 11, //this is always one more than how many you want
+  row: 30,
+  factor: 2,
+  tile: 'stair',
+  layer: 0,
+  erase: false,
+}
+
 export {
-  tiles
+  elements,
+  tiles,
+  settings
 }
--- isoDraw/scripts/iso.js
+++ isoDraw/scripts/iso.js
@@ -1,30 +1,8 @@
-import { tiles } from './data.js'
+import { elements, tiles, settings } from './data.js'
 
 function init() { 
-
-  // TODO add guidelines to canvas background
-  // TODO add eraser (filter if x and y matches)
-
-  const elements = {
-    canvas: {
-      el: document.querySelector('canvas'),
-      ctx: function() {
-        return this.el.getContext('2d')
-      }
-    },
-    line: document.querySelector('.line'),
-    cube: document.querySelector('.cube'),
-    stair: document.querySelector('.stair'),
-    blank: document.querySelector('.blank'),
-    btns: document.querySelectorAll('.btn'),
-    indicator: document.querySelector('.indicator'),
-    cellData: document.querySelectorAll('.cells'),
-    stamp: document.querySelector('.stamp'),
-    palette: document.querySelector('.palette'),
-    max: document.querySelector('.max'),
-    layers: document.querySelector('.layers'),
-  }
-
+  
+  // TODO add ways to download layers into one sheet
 
   const input = {
     layer: document.querySelector('.layer'),
@@ -38,21 +16,9 @@ function init() {
     input[key].addEventListener('change', ()=> {
       if (state.cells.length > input[key].value) settings[key] = +input[key].value
       input[key].value = settings[key]
-      console.log(settings, state.cells)
     })
   })
 
-
-  const settings = {
-    column: 11, //this is always one more than how many you want
-    row: 30,
-    factor: 2,
-    tile: 'stair',
-    layer: 0
-  }
-
-  // const calcX = i => i % settings.column
-  // const calcY = i => Math.floor(i / settings.column)
   const nearestN = (n, denom) =>{
     return n === 0 ? 0 : (n - 1) + Math.abs(((n - 1) % denom) - denom)
   }
@@ -69,7 +35,7 @@ function init() {
     if (w) el.style.width = w
     el.style.transform = `translate(${x ? px(x) : 0}, ${y ? px(y) : 0})`
   }
-  
+
 
   resizeCanvas({
     canvas: elements.canvas.el,
@@ -80,10 +46,12 @@ function init() {
 
 
   const updateCanvas = () => {
+    const { width, height } = elements.canvas.el
+    elements.canvas.ctx().clearRect(0, 0, width, height)
+
     state.cells.forEach(layer => {
       const { factor } = settings
       layer.forEach(c => {
-        // const offset = calcY(i) % 2 === 0 ? 0 : (18 * factor)
       elements.canvas.ctx().drawImage(
         Array.from(document.querySelectorAll('.palette-tile')).find(tile => tile.dataset.id === c.img),
         c.x * factor, 
@@ -94,24 +62,24 @@ function init() {
   }
   
 
-
-
-
   elements.canvas.el.addEventListener('click', () => {
     const { left, top } = elements.canvas.el.getBoundingClientRect()
     const { x: sX, y: sY } = elements.stamp.getBoundingClientRect()
     const { factor, layer } = settings
 
-    console.log('layer', layer, elements.cellData, settings)
-
     const x = (sX - left) / factor
     const y = (sY - top) / factor
+  
     state.cells[layer] = state.cells[layer].filter(c => c.x !== x || c.y !== y)
-    state.cells[layer].push({
-      img: settings.tile,
-      x, y
-    })
-    state.cells[layer].sort((a, b) => a.y - b.y)
+
+    if (!settings.erase) {
+      state.cells[layer].push({
+        img: settings.tile,
+        x, y
+      })
+      state.cells[layer].sort((a, b) => a.y - b.y)
+    }
+
     elements.cellData[layer].value = state.cells[layer].map(c => `${c.img}.${c.x}-${c.y}`)
     elements.indicator.innerHTML = state.cells[layer].length
     updateCanvas()
@@ -129,22 +97,26 @@ function init() {
     })
   })
 
+  elements.canvas.el.addEventListener('mouseover', ()=> elements.stamp.classList.remove('d-none'))
+  elements.canvas.el.addEventListener('mouseleave', ()=> elements.stamp.classList.add('d-none'))
+
   window.addEventListener('mousemove', e => {
-    const { left, top, width, height } = elements.canvas.el.getBoundingClientRect()
+    const { left, top } = elements.canvas.el.getBoundingClientRect()
     const { pageX: pX, pageY: pY } = e
 
-    elements.stamp.classList[
-      pX < left || pY < top || pX > (left + width) || pY > (top + height) ? 'add' : 'remove'
-    ]('d-none')
     const cX = 18 * 2
     const cY = 9 * 2
-    const x = nearestN(pX - left - window.scrollX, cX) - cX + left
-    const y = nearestN(pY - top - window.scrollY, cY) - cY + top
+    const x = nearestN(pX - left - window.scrollX, cX) - cX + left + window.scrollX
+    const y = nearestN(pY - top - window.scrollY, cY) - cY + top + window.scrollY
     setStyles({
       el: elements.stamp,
       x, y
     })
-    elements.stamp.setAttribute('data-coord', `${x - left}-${y - top}`) // TODO this might not be in sync
+
+    elements.stamp.setAttribute(
+      'data-coord', 
+      `${(x - left - window.scrollX) / settings.factor}-${(y - top - window.scrollY) / settings.factor}`
+      )
     elements.stamp.innerHTML = `<img src="${tiles[settings.tile]}">`
   })
 
@@ -155,18 +127,20 @@ function init() {
     link.click()
   }
 
-  const addBtnAction = (btn, actionName, action) => {
-    if (btn.dataset.action === actionName) {
-      btn.addEventListener('click', action)
-    }
-  }
+
 
   elements.btns.forEach(btn => {
-    addBtnAction(btn, 'download', download)
-    addBtnAction(btn, 'add-layer', ()=> {
+    const addBtnAction = (actionName, action) => {
+      if (btn.dataset.action === actionName) {
+        btn.addEventListener('click', action)
+      }
+    }
+    
+    addBtnAction('download', download)
+    addBtnAction('add-layer', ()=> {
       state.cells.push([])
       elements.max.innerHTML = state.cells.length - 1
-      // <textarea class="cells" data-index="1" spellcheck="false"></textarea> 
+
       const cellsInput = Object.assign(document.createElement('textarea'), {
         value: state.cells.length - 1,
         className: 'cells',
@@ -175,6 +149,24 @@ function init() {
       cellsInput.setAttribute('data-index', state.cells.length - 1)
       elements.layers.appendChild(cellsInput)
       elements.cellData = document.querySelectorAll('.cells')
+      settings.layer = state.cells.length - 1
+      input.layer.value = settings.layer  
+    })
+    addBtnAction('layer-up', ()=> {
+      settings.layer = settings.layer < state.cells.length - 1 
+        ? settings.layer + 1 
+        : 0
+      input.layer.value = settings.layer  
+    })
+    addBtnAction('layer-down', ()=> {
+      settings.layer = settings.layer === 0 
+        ? state.cells.length - 1
+        : settings.layer - 1
+      input.layer.value = settings.layer  
+    })
+    addBtnAction('erase', ()=> {
+      settings.erase = !settings.erase
+      elements.stamp.classList[settings.erase ? 'add' : 'remove']('erase-mode')
     })
   })
 
