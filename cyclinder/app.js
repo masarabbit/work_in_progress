0567f2da65d0c82d2c4dac59b4cb773461711fdb
Masa Takeuchi <masahitoart@gmail.com>
2024-06-06 23:44:32 +0200

    add further feature to cylinder cat

--- cyclinder/app.js
+++ cyclinder/app.js
@@ -8,7 +8,19 @@
       if (isNum(w)) el.style.width = px(w)
       if (isNum(h)) el.style.height = px(h)
       el.style.transform = `translate(${x ? px(x) : 0}, ${y ? px(y) : 0}) rotate(${deg || 0}deg)`
-      // el.style.zIndex = y
+    }
+
+
+    const addEvents = (target, event, action, array) => {
+      array.forEach(a => event === 'remove' ? target.removeEventListener(a, action) : target.addEventListener(a, action))
+    }
+
+    const mouse = {
+      up: (t, e, a) => addEvents(t, e, a, ['mouseup', 'touchend']),
+      move: (t, e, a) => addEvents(t, e, a, ['mousemove', 'touchmove']),
+      down: (t, e, a) => addEvents(t, e, a, ['mousedown', 'touchstart']),
+      enter: (t, e, a) => addEvents(t, e, a, ['mouseenter', 'touchstart']),
+      leave: (t, e, a) => addEvents(t, e, a, ['mouseleave'])
     }
 
     const normalisedAngle = deg => {
@@ -24,54 +36,130 @@
       deg: 0,
       front: document.querySelector('.front'),
       back: document.querySelector('.back'),
-      catFace: document.querySelector('.cat-face'),
+      catElements: document.querySelectorAll('.cat-el'),
       x: 0,
       y: 0,
-      marker: document.querySelector('.marker')
+      panels: document.querySelectorAll('.panel'),
+      roll: {
+        x: 0,
+        y: 0,
+      }
+    }
+
+    const drag = (el, pos, x, y) =>{
+      pos.a = pos.c - x
+      pos.b = pos.d - y
+      const newX = el.offsetLeft - pos.a
+      const newY = el.offsetTop - pos.b
+
+      cylinder.roll.x = newX > el.offsetLeft ? -4 : 4
+      cylinder.roll.y = newY >el.offsetTop ? -4 : 4
+    }
+
+    const client = (e, type) => e.type[0] === 'm' ? e[`client${type}`] : e.touches[0][`client${type}`]
+    const roundedClient = (e, type) => Math.round(client(e, type))
+
+    const addTouchAction = el =>{
+      const pos = { a: 0, b: 0, c: 0, d: 0 }
+      
+      const onGrab = e =>{
+        pos.c = roundedClient(e, 'X')
+        pos.d = roundedClient(e, 'Y')  
+        mouse.up(document, 'add', onLetGo)
+        mouse.move(document, 'add', onDrag)
+      }
+      const onDrag = e =>{
+        const x = roundedClient(e, 'X')
+        const y = roundedClient(e, 'Y')
+        drag(el, pos, x, y)
+        pos.c = x
+        pos.d = y
+        moveCylinder(e)
+      }
+      const onLetGo = () => {
+        mouse.up(document, 'remove', onLetGo)
+        mouse.move(document,'remove', onDrag)
+      }
+      mouse.down(el,'add', onGrab)
     }
 
 
+    const moveCylinder = () => {
+      console.log('moveCylinder')
+
     
-    cylinder.el.addEventListener('click', e => {
-      // cylinder.deg += 45
-      const { x, y } = cylinder.marker.getBoundingClientRect()
-      // this amount should be calculated with PI in mind
-      const yDiff = y < e.pageY ? 90 : -90
-      const xDiff = x < e.pageX ? 90 : -90
-    
+      const { roll } = cylinder
+        // angle: -
+      if (cylinder.deg % 180 === 90) {
+        cylinder.y -= roll.y
+        // angle: |
+      } else if (cylinder.deg % 180 === 0) {
+        cylinder.x -= roll.x
+      } else {
+        //  angle: \
+        if (cylinder.deg % 180 === 135
+            && (
+                (roll.x < 0 && roll.y > 0) ||
+                (roll.x > 0 && roll.y < 0)
+              )){
+                  cylinder.x -= roll.x
+                  cylinder.y -= roll.y
+                }
+        //  angle: /
+        if (cylinder.deg % 180 === 45 
+          && (
+              (roll.x > 0 && roll.y > 0) ||
+              (roll.x < 0 && roll.y < 0)
+            )){
+                cylinder.x -= roll.x
+                cylinder.y -= roll.y
+              }
+      }
+
+      setStyles(cylinder)
+      const elAngle = cylinder.deg % 180 === 90 
+        ? cylinder.y % 360
+        : cylinder.x % 360
+
+      const adjustedAngle = normalisedAngle(cylinder.deg)
+      cylinder.catElements.forEach(el => {
+        setStyles({ el, deg: adjustedAngle > 90 && adjustedAngle <= 270  ? elAngle * -3 : elAngle * 3})
+      })
+
+    }
+
+    addTouchAction(cylinder.el)
+
+    const spinCat = () => {
+      cylinder.deg += 45
       const adjustedAngle = normalisedAngle(cylinder.deg)
 
       if (cylinder.deg % 180 === 90) {
         cylinder.h = 100
-        cylinder.y -= yDiff
-    
       } else if (cylinder.deg % 180 === 0) {
         cylinder.h = 60
-        cylinder.x -= xDiff
       } else {
         cylinder.h = 80
-        cylinder.x -= xDiff
-        cylinder.y -= yDiff
       }
-
       setStyles(cylinder)
-      rotateX({ el: cylinder.front, deg: cylinder.deg })
-      rotateX({ el: cylinder.back, deg: cylinder.deg })
-
-      const faceAngle = cylinder.x % 360
-      setStyles({ el: cylinder.catFace, deg: faceAngle * 3})
+      cylinder.panels.forEach(el => {
+        rotateX({ el, deg: cylinder.deg })
+      })
 
       cylinder.el.classList[
         adjustedAngle > 90 && adjustedAngle <= 270 
           ? 'add'
           : 'remove'
       ]('flip')
-      
-  
 
       document.querySelector('.indicator').innerHTML = `${adjustedAngle}deg  ${cylinder.deg % 180}deg
       `
+    }
+    
+    ;[cylinder.front, cylinder.back].forEach(el => {
+      el.addEventListener('click', spinCat)
     })
+    document.querySelector('button').addEventListener('click', spinCat)
   }
   
   window.addEventListener('DOMContentLoaded', init)
